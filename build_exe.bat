f0432a0c3ca3e93c626718cf95abe6923cce4861
aaron <aaron@hotcoin.biz>
2026-08-14 10:35:28 +0800

    修改打包的文件名

--- build_exe.bat
+++ build_exe.bat
@@ -7,9 +7,9 @@ REM 使用PyInstaller打包，--windowed表示不显示控制台窗口，--onefi
 REM 将模板和默认配置一并嵌入单文件EXE
 set "PYINSTALLER=.venv\Scripts\pyinstaller.exe"
 if not exist "%PYINSTALLER%" set "PYINSTALLER=pyinstaller"
-"%PYINSTALLER%" --clean --noconfirm --onefile --name="游戏机器人" --add-data "templates;templates" --add-data "config.json;." game_bot.py
+"%PYINSTALLER%" --clean --noconfirm --onefile --name="start_game" --add-data "templates;templates" --add-data "config.json;." game_bot.py
 
 echo.
 echo 打包完成！
-echo 可执行文件位于 dist/游戏机器人.exe
+echo 可执行文件位于 dist/start_game.exe
 pause
